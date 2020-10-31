1bde9df45cf36b110f80ccee736fe1b3f962f78b
myryricha5 <risa_yasui@mail.benesse.co.jp>
2020-10-31 22:38:16 +0900

    Swift commit

---
+++ Hello World Project/Hello World Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello World Project
+//
+//  Created by 安井梨沙子 on 2020/10/31.
+//  Copyright © 2020 example.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
