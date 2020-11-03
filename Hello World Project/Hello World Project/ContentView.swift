ceba475e0bc0a35def8c1bb4253a1fabd8651278
myryricha5 <risa_yasui@mail.benesse.co.jp>
2020-11-03 21:57:14 +0900

    Change image modifire

--- Hello World Project/Hello World Project/ContentView.swift
+++ Hello World Project/Hello World Project/ContentView.swift
@@ -14,16 +14,42 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text(labelText)
+                .foregroundColor(.white)
+                .background(Color.black)
+                .padding(50)
+                .background(Color.yellow)
                 .font(.largeTitle)
-                .foregroundColor(Color.red)
+                .clipShape(Circle())
+                .overlay(
+                    Color.green
+                        .offset(x:10, y:10))
+                .overlay(
+                    Text("Hacking to the Swift")
+                        .background(Color.pink)
+                        .offset(x:20,y:20))
+                
             Button(action:{self.labelText = "Yes Tapped!"}){
                 Text("Let's Tap!")
                     .font(.footnote)
                     .fontWeight(.thin)
                     .foregroundColor(Color.black)
                     .frame(width: 70, height: 20)
-                    .border(Color.gray)
+                    .border(Color.gray, width: 2)
             }
+            Text(String(repeating: "Hello World ", count: 20))
+                .lineLimit(3)//表示行数を指定
+                .truncationMode(.middle)//省略位置を指定
+                .lineSpacing(20)//行間を調整
+            Image(systemName: "circle.grid.hex.fill")
+                .font(.largeTitle)
+                .foregroundColor(.orange)
+            Image("setting")
+                .resizable()
+                .scaledToFill()
+                .frame(width:100, height: 50)
+                .clipped()
+                        
+            
         }
     }
 }
